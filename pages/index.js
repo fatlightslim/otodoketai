23bca38b2bdb993acf393629d9d434a9c7d4c93a
abiru <masataka.abiru@gmail.com>
2021-02-22 10:48:48 +0900

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import Header from "./components/Header"
+// import Header from "./components/Header"
 import Image from "next/image"
 
 const shops = [
