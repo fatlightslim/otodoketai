f9c5b60433eb242ec594090e53c973ab0b8c3509
abiru <masataka.abiru@gmail.com>
2021-06-02 11:45:26 +0900

    fix: holidays

--- components/cart/CartDetail.js
+++ components/cart/CartDetail.js
@@ -57,7 +57,6 @@ const Summary = ({ dateNumber }) => {
       {items.map((v) => {
         const { fields, holidays, sys } = v
         const { title, image, price } = fields
-        console.log(holidays);
         return (
           <div
             key={v.sys.id}
