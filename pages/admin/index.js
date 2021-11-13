3fb978a2cbc9e5e38aa1ce80d22b449ed9aaf5a6
fatlightslim <masataka.abiru@gmail.com>
2021-11-13 11:09:06 +0900

    fix

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -103,16 +103,17 @@ const Stats = ({ data }) => {
 export async function getServerSideProps() {
   // Fetch data from external API
   const url = process.env.URL || "http://localhost:3000"
-  const res = await fetch(`${url}/api/orders`)
-  const data = await res.json()
+  // const res = await fetch(`${url}/api/orders`)
+  // const data = await res.json()
 
-  const filtered = data.filter(
-    (v) => {
-      const status = v.log.slice(-1)[0]["status"] 
-      const blacklist = ['draft', 'cancel']
-      return  !blacklist.includes(status)
-    }
-  )
+  // const filtered = data.filter(
+  //   (v) => {
+  //     const status = v.log.slice(-1)[0]["status"] 
+  //     const blacklist = ['draft', 'cancel']
+  //     return  !blacklist.includes(status)
+  //   }
+  // )
+  const filtered = []
   return {
     props: { data: filtered },
   }
